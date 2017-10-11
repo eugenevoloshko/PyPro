e924b88aa01c42b21daa1c5deadc1da461e06690
Eugene <dizx.ubuntu@gmail.com>
2017-10-11 12:45:04 +0300

    выходной файл! осталась проблема с пробелами между символами.

--- workWithTextFile.py
+++ workWithTextFile.py
@@ -17,5 +17,22 @@ for i in a:
     s = a[ii]
     n = s.find('M')
     if n != -1:
-        print(s)
+        slotN = s[0:14]
+        print(slotN)
+
+        barCode = s[18:34]
+        print(barCode)
+
+        dateExp = s[36:56]
+        print(dateExp)
+
+        l = len(s)
+
+        typeOfData = s[76:l]
+        print(typeOfData)
+
+        ltr = open('listToRemove.txt', 'a')
+
+        ltr.write(slotN + '\n' + 'Barcode: ' + barCode + '\n' + 'Expire in: ' + dateExp + '\n' + 'Content: ' + typeOfData + '\n' + '\n')
+
     ii = ii + 1
